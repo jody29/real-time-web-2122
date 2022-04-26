b11156ee4b396c7764f0d2f8e98ae6ac3151db19
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-04-26 18:46:09 +0200

    first page works

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 require('dotenv').config()
 const express = require('express')
+const http = require('http')
+const PORT = process.env.PORT || 5500
+const bodyParser = require('body-parser')
+const path = require('path')
+
 const app = express()
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+
+const homeRouter = require('./src/routers/homeRouter.js')
+
+// express setup
+app
+.set('view engine', 'ejs')
+.set('views', path.join(__dirname, 'src/views'))
+.use(express.static(__dirname + '/public'))
+.use(bodyParser.urlencoded({ extended: true }))
+.use('/', homeRouter)
+
+// socket connection
+io.on('connection', socket => {
+    console.log('user connected')
+})
+
+server.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
+
+
+
+
 
